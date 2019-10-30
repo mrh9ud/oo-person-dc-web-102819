84c1b527708488af388800b1b411bcaf66085cad
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-30 20:06:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -31,39 +31,6 @@ class Person
     end
     
     def happy?
-        if @happi# your code goes here
-class Person
-    @@all = []
-    attr_accessor :name, :happiness, :hygiene, :account
-    def initialize(name, account=25, happiness=8, hygiene=8)
-        @name = name
-        @account = account
-        @happiness = happiness
-        @hygiene = hygiene
-        @@all << self
-    end
-
-    def self.all
-        @@all
-    end
-    
-    def get_paid(salary)
-        @account += salary
-        "All about the benjamins"
-    end
-
-   
-    def happiness=(happiness)
-        if happiness >= 0 && happiness <= 10
-          @happiness = happiness
-        elsif happiness > 10
-           @happiness = 10
-        else 
-            @happiness = 0
-        end
-    end
-    
-    def happy?
         if @happiness > 7 && @happiness <= 10
             return true
         else
@@ -72,39 +39,19 @@ class Person
     end
 
     def hygiene=(hygiene_level)
-      if hygiene_level >= 0 && hygiene_level <= 10
-          @hygiene = hygiene_level
-        elsif hygiene_level > 10
-           @hygiene = 10
-        else 
-            @hygiene = 0
-      end
+        if hygiene_level >= 0 && hygiene_level <= 10
+            @hygiene = hygiene_level
+          elsif hygiene_level > 10
+             @hygiene = 10
+          else 
+              @hygiene = 0
+          end
     end
 
     def clean?
         if @hygiene > 7 && @hygiene <= 10
             return true
         else
-            return false
-        end
-    end
-    
-endness > 7 && @happiness <= 10
-            return true
-        else
-            return false
-        end
-    end
-
-
-    def clean?
-        if @hygiene > 7 && @hygiene <= 10 && @hygiene >= 0
-            return true
-        elsif @hygiene > 10
-            @hygiene = 10
-        elsif @hygiene < 0
-            @hygiene = 0
-        else 
             return false
         end
     end
